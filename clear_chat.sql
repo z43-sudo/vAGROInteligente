dede63b39c4069cbc8ee32d6976095be122cf882
Alisson <wallisom_53@outlook.com>
2025-12-04 10:17:35 -0300

    redep

--- clear_chat.sql
+++
@@ -1,5 +0,0 @@
--- Limpar todas as mensagens do chat
-TRUNCATE TABLE messages;
-
--- OU, se preferir apagar apenas de uma fazenda específica (substitua o ID):
--- DELETE FROM messages WHERE farm_id = 'farm_123...';
